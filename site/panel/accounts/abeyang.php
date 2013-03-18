702c7ccc6b922de8a9e9d9f0140f4f61b0a967da
Abe Yang <abeyang@gmail.com>
2013-03-17 22:00:48 -0700

    Initial checkin

---
+++ site/panel/accounts/abeyang.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# setup your account credentials here
+# it's highly recommended to use md5 or sha1 encryption 
+# for your passwords. read more about encryption in the
+# docs: http://getkirby.com/docs/panel/accounts
+
+# From Abe: use md5 generator:
+# http://md5generator.net/
+
+username: abeyang
+password: e89c1c670d5582665546e38056d061e1
+encrypt:  md5
+language: en
---
+++ site/templates/articles.php
@@ -0,0 +1,58 @@
+<?php snippet('header') ?>
+
+<?php snippet('navigation') ?>
+
+<!--Article-->
+<div class="grid">
+	<?php if(param('tag')) {
+	
+	  $articles = $pages->find('articles')
+	                    ->children()
+	                    ->visible()
+	                    ->filterBy('tags', param('tag'), ',')
+	                    ->flip()
+	                    ->paginate(10);
+	
+	} else {
+	
+	  $articles = $pages->find('articles')
+	                    ->children()
+	                    ->visible()
+	                    ->flip()
+	                    ->paginate(10);
+	
+	} ?>
+	<?php foreach($articles as $article): ?>
+	<div class="row">		
+		<div class="slot-6-7-8-9">
+			<p class="date"><?php echo html($article->published()) ?></p>
+			<h1 class="article-title"><a href="<?php echo $article->url() ?>"><?php echo html($article->title()) ?></a></h1>
+			<p>
+				<?php echo $article->description() ?> <?php echo excerpt($article->text(), 800) ?> <a href="<?php echo $article->url() ?>">Read more</a>
+			</p>			
+		</div>
+	</div>
+	<?php endforeach ?>
+	<?php if($articles->pagination()->hasPages()): ?>
+	<div class="row">		
+		<div class="slot-6-7-8-9">
+			<p>    						
+				<?php if($articles->pagination()->hasPrevPage()): ?>
+				<a class="prev" href="<?= $articles->pagination()->prevPageURL() ?>">« Previous</a>
+				<?php endif ?>
+				
+				<?php if($articles->pagination()->hasPrevPage() && $articles->pagination()->hasNextPage()): ?>
+				|
+				<?php endif ?>
+				
+				<?php if($articles->pagination()->hasNextPage()): ?>
+				<a class="next" href="<?= $articles->pagination()->nextPageURL() ?>">Next  »</a>
+				<?php endif ?>
+			</p>			
+		</div>
+	</div>
+	<?php endif ?>
+</div>
+
+
+<?php snippet('footer') ?>
