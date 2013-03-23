d773467c7bfc2b1fcdc129d246d37a390ca3cffe
Abe Yang <abeyang@gmail.com>
2013-03-23 15:07:18 -0700

    Entered in BKoo as user

---
+++ site/panel/accounts/bkoo.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# setup your account credentials here
+# it's highly recommended to use md5 or sha1 encryption 
+# for your passwords. read more about encryption in the
+# docs: http://getkirby.com/docs/panel/accounts
+
+# From Abe: use md5 generator:
+# http://md5generator.net/
+
+username: bkoo
+password: d7a84628c025d30f7b2c52c958767e76
+encrypt:  md5
+language: en
