497e85df3d70bd8de0507190321eeb0bc92737dd
Abe Yang <abeyang@gmail.com>
2013-03-23 15:10:11 -0700

    Added Jordan and Ivan as users

---
+++ site/panel/accounts/iagui005.php
@@ -0,0 +1,15 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# setup your account credentials here
+# it's highly recommended to use md5 or sha1 encryption 
+# for your passwords. read more about encryption in the
+# docs: http://getkirby.com/docs/panel/accounts
+
+# From Abe: use md5 generator:
+# http://md5generator.net/
+
+name: Ivan Aguirre
+username: iagui005
+password: b288cb1e6c3abc338bec3b66cee5c39f
+encrypt:  md5
+language: en
---
+++ site/panel/accounts/tajordan88.php
@@ -0,0 +1,15 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# setup your account credentials here
+# it's highly recommended to use md5 or sha1 encryption 
+# for your passwords. read more about encryption in the
+# docs: http://getkirby.com/docs/panel/accounts
+
+# From Abe: use md5 generator:
+# http://md5generator.net/
+
+name: Jordan Ta
+username: tajordan88
+password: f43f15643b8c185fa3af753c7267eef5
+encrypt:  md5
+language: en
