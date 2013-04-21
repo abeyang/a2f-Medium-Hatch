fdee6bb4473be5533873b17efa7c4d7868032849
Abe Yang <abeyang@gmail.com>
2013-04-21 10:32:46 -0700

    Added Font Awesome. Moved a2f LESS file to new location

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -56,14 +56,8 @@
 
 	<meta itemprop="name" content="<?= html($page->title()) ?> | <?= html($site->title()) ?>">
 	<meta itemprop="description" content="<?= html($page->description()) ?>">
-
-	<!-- Styles -->
-	<!-- <link rel="stylesheet" href="<?= url('assets/css/base.css') ?>" type="text/css" media="all" />
-	<link rel="stylesheet" href="<?= url('assets/css/720_grid.css') ?>" type="text/css" media="screen and (min-width: 720px)" />
-	<link rel="stylesheet" href="<?= url('assets/css/986_grid.css') ?>" type="text/css" media="screen and (min-width: 986px)" />
-	<link rel="stylesheet" href="<?= url('assets/css/1236_grid.css') ?>" media="screen and (min-width: 1236px)" /> -->
 		
-	<link rel="stylesheet/less" type="text/css" href="<?= url('assets/bootstrap/less/a2f.bootstrap.less') ?>" />
+	<link rel="stylesheet/less" type="text/css" href="<?= url('assets/styles/a2f.bootstrap.less') ?>" />
     <script type="text/javascript" src="<?= url('assets/js/less-1.3.3.min.js') ?>"></script>
     
     <script type="text/javascript">
