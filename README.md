7f10287f804650808c5c1e25dafb25b36ad1d106
Jordan Ta <tajordan88@unknown0026bb07a175.gateway.2wire.net>
2013-04-21 10:30:51 -0700

    sidebar.php Edit
    Added 3 words: "a2fRiverside", "About Us", and "Archives"

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 a2f-Medium-Hatch
 ================
 
-act2fellowship 2013 theme. Uses Kirby CMS. Testing commit.
+act2fellowship 2013 theme. Uses Kirby CMS. 
--- site/snippets/sidebar.php
+++ site/snippets/sidebar.php
@@ -5,12 +5,23 @@
 ?>
 
 <aside class="sidebar">
-	<div>Sidebar</div>
-    <a class="brand" href="<?= html($site->url()) ?>"><?= html($site->title()) ?></a>
+<!--    <a class="brand" href="<?= html($site->url()) ?>"><?= html($site->title()) ?></a>
     <ul>
 		<? foreach($pages->visible() as $p): ?>
 		<li><a href="<?= $p->url() ?>" class="nav"><?= html($p->title()) ?></a></li>
 		<? endforeach ?>	
+    </ul> -->
+    
+    <ul class="menu">
+    	<li>
+    		<a>a2f Riverside</a>
+    	</li>
+    	<li>
+    		<a>About Us</a>
+    	</li>
+    	<li>
+    		<a>Archives</a>
+    	</li>
     </ul>
 </aside>
 
