8fc3e6268964f15da715fbbea9f6feefcae49364
Andrew Robertson <arobe004@ucr.edu>
2013-04-21 12:16:38 -0700

    in progress post page

--- site/templates/post.php
+++ site/templates/post.php
@@ -10,39 +10,58 @@ snippet('header');
 <?php snippet('sidebar') ?>
 
 <!--post-->
-<div class="wrapper">
+<div class="post">
 	<div class="container">	
 		<div class="row">		
-			<div class="span9">
-				<p class="date"><?php echo html($page->published()) 
-				//translate_date($page->published())?></p>
+			<div class="span12">
+					<div class="row"
+						<div class="span8 offset2">
+							<img class="picture" src="http://farm8.staticflickr.com/7259/7812836158_f6e6232df2_c.jpg" alt="10k"></img>
+						</div>
+					</div>	
+			
 
-				<h1 class="post-title"><a href="<?php echo $page->url() ?>"><?php echo html($page->title()) ?></a></h1>			
-				<?php echo kirbytext($page->text()) ?>
-				<p>
-					<?php snippet('share') ?><br /><br />
-					URL: <a id="url" href="<?php echo html($page->url()) ?>"><?php echo html($page->url()) ?></a><br />
-					Tags: <?php foreach(str::split($page->tags()) as $tag): ?>
-					<a href="<?php echo url('posts/tag:' . urlencode($tag)) ?>">#<?php echo html($tag) ?></a>
-					<?php endforeach ?>
-				</p>
-				<p>    						
-					<?php if($page->hasPrev()): ?>
-					<a href="<?php echo $page->prev()->url() ?>">« Previous</a>
-					<?php endif ?>
+					<div class="row">
+						<div class="row">
+							<h1 class="post-title span8 offset2"><a href="<?php echo $page->url() ?>"><?php echo html($page->title()) ?></a></h1>			
+						</div>
+						<div class="row">
+							<?php echo kirbytext($page->text()) ?>
+						</div>	
+						<div class="row">
+							<p class="date"><?php echo html($page->published()) 
+							//translate_date($page->published())?></p>
+						</div>
+						
+						<p>
+							<?php snippet('share') ?><br /><br />
+							URL: <a id="url" href="<?php echo html($page->url()) ?>"><?php echo html($page->url()) ?></a><br />
+							Tags: <?php foreach(str::split($page->tags()) as $tag): ?>
+							<a href="<?php echo url('posts/tag:' . urlencode($tag)) ?>">#<?php echo html($tag) ?></a>
+							<?php endforeach ?>
+						</p>
+					</div>
 					
-					<?php if($page->hasPrev() && $page->hasNext()): ?>
-					|
-					<?php endif ?>
 					
-					<?php if($page->hasNext()): ?>
-					<a href="<?php echo $page->next()->url() ?>">Next »</a>
-					<?php endif ?>
-				</p>		
-			</div>
-		</div>	
+						<p>    						
+						<?php if($page->hasPrev()): ?>
+						<a href="<?php echo $page->prev()->url() ?>">« Previous</a>
+						<?php endif ?>
+						
+						<?php if($page->hasPrev() && $page->hasNext()): ?>
+						|
+						<?php endif ?>
+						
+						<?php if($page->hasNext()): ?>
+						<a href="<?php echo $page->next()->url() ?>">Next »</a>
+						<?php endif ?>
+						</p>		
+		</div>		
+				
+		
+		
 		<div class="row">		
-			<div class="span9">
+			<div class="span12">
 				<?php snippet('disqus', array('disqus_shortname' => $site->disqus())) ?>						
 			</div>
 		</div>
